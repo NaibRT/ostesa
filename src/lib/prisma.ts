19845411bf14c0c8270c8d1b4551d268dfd3343a
NaibRT <naibtahmazli@gmail.com>
2024-09-02 00:46:29 +0200

    changed prismaclient to nodemodules

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,14 @@
-// import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from "../PrismaClient";
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "../PrismaClient";
+
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+if (process.env.NODE_ENV === "development"){ globalForPrisma.prisma = new PrismaClient();} 
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["query"],
   });
-
-if (process.env.NODE_ENV != "production") globalForPrisma.prisma;
